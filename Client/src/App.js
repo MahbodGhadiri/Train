f908363998f569696a99ba44e6133c6a3c6d6418
Mahbod <m.ghadirisani2013@gmail.com>
2021-12-30 21:57:49 +0330

    Added "showError" and "showInfo" features

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Train from './components/Train';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <Router>
-        <Train/>
+          <Train/>
+        <ToastContainer />
       </Router>
     </>
   );
